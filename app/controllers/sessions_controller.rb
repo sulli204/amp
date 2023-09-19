15e2e9624f5767719dbfbea422334c61b9b3a4f0
Patrick Sullivan <patrick@tanda.co>
2023-09-19 16:38:50 -0500

    Login and logout have basic tests and work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      render :new
+      flash[:alert] = "Incorrect email or password"
+      render :new, status: :unprocessable_entity
     end
   end
 
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,42 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "login fails on bad password" do
+    auth_params = {
+      authenticity_token: "token",
+      email: "bad@email.com",
+      password: "don'texist"
+    }
 
+    post login_path(auth_params)
+    assert_response :unprocessable_entity
+  end
+
+  test "login works on created user" do
+    user_params = {
+      user: {
+        name: "New User",
+        email: "newuser@amp.com",
+        username: "newuser",
+        password: "password",
+        password_confirmation: "password"
+      }
+    }
+
+    auth_params = {
+      email: "newuser@amp.com",
+      password: "password"
+    }
+
+    assert_difference 'User.count' do
+      post users_path(params: user_params)
+      assert_response :redirect
+    end
+
+    delete logout_path
+    assert_redirected_to root_path
+
+    post login_path(auth_params)
+    assert_redirected_to root_path
+  end
 end
