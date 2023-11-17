3acb7e4c299fe3b0f59c3267822278d48ebfd729
Patrick Sullivan <patrick@tanda.co>
2023-11-17 13:04:27 -0600

    Clicking on the same song won't stop the music

--- app/javascript/controllers/song_controller.js
+++ app/javascript/controllers/song_controller.js
@@ -8,8 +8,14 @@ export default class extends Controller {
 
 
   async play() {
-    debugger
     let audioPlayer = document.getElementById("audio-player")
-    audioPlayer.setAttribute("src", "https://pub-527d9c7ea2cd4de4ba5105088eb10f97.r2.dev/" + this.pathValue)
+    let current_song = audioPlayer.getAttribute("song")
+
+    if (current_song === this.pathValue) {
+      return
+    } else {
+      audioPlayer.setAttribute("song", this.pathValue)
+      audioPlayer.setAttribute("src", "https://pub-527d9c7ea2cd4de4ba5105088eb10f97.r2.dev/" + this.pathValue)
+    }
   }
 }
