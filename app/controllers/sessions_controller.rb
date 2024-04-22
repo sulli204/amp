96ec5e9e6375975b3fc5caba3524ee96b5b337a3
Patrick Sullivan <patrick@tanda.co>
2024-04-22 10:22:54 -0500

    Songs playing, pausing, and switch ui and functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
+      flash[:alert] ? "" : ""
       session[:user_id] = user.id
       redirect_to root_path
     else
--- app/javascript/controllers/song_controller.js
+++ app/javascript/controllers/song_controller.js
@@ -12,26 +12,66 @@ export default class extends Controller {
 
   load() {
     let audioPlayer = document.getElementById("audio-player")
-    let current_song = audioPlayer.getAttribute("song")
+    let current_song_id = audioPlayer.getAttribute("song")
 
-    if (current_song === this.pathValue) {
-      audioPlayer.paused ? this.play(audioPlayer) : this.pause(audioPlayer)
+    let selected_song_id = this.idValue
+
+    if (current_song_id) {
+      if (current_song_id == selected_song_id) {
+        if (audioPlayer.paused) {
+          this.unpause(selected_song_id)
+        } else {
+          this.pause()
+        }
+      } else {
+        this.swtich_songs(current_song_id)
+      }
+
     } else {
-      audioPlayer.setAttribute("song", this.pathValue)
-      audioPlayer.setAttribute("src", this.playbackUrlValue + this.pathValue)
-      this.play(audioPlayer)
+      this.start(selected_song_id)
     }
   }
 
-  play(audioPlayer) {
+  start() {
+    let audioPlayer = document.getElementById("audio-player")
+    console.log("Starting song " + this.idValue)
+
     this.playTarget.classList.add(this.hideClass)
     this.pauseTarget.classList.remove(this.hideClass)
+
+    audioPlayer.setAttribute("song", this.idValue)
+    audioPlayer.setAttribute("src", this.playbackUrlValue + this.pathValue)
+
     audioPlayer.play()
   }
 
-  pause(audioPlayer) {
-    this.pauseTarget.classList.add(this.hideClass)
+  unpause() {
+    let audioPlayer = document.getElementById("audio-player")
+
+    this.playTarget.classList.add(this.hideClass)
+    this.pauseTarget.classList.remove(this.hideClass)
+
+    audioPlayer.play()
+  }
+
+  pause() {
+    let audioPlayer = document.getElementById("audio-player")
+
     this.playTarget.classList.remove(this.hideClass)
+    this.pauseTarget.classList.add(this.hideClass)
+
     audioPlayer.pause()
+  }
+
+  swtich_songs(current_song_id) {
+    let audioPlayer = document.getElementById("audio-player")
+
+    let current_song_play_icon = document.getElementById(current_song_id + "-play-icon")
+    let current_song_pause_icon = document.getElementById(current_song_id + "-pause-icon")
+
+    current_song_play_icon.classList.remove(this.hideClass)
+    current_song_pause_icon.classList.add(this.hideClass)
+
+    this.start()
   }
 }
