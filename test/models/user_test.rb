774461043a156c36cfa0b278166273c66173ddf2
Patrick Sullivan <patrick@tanda.co>
2024-09-19 19:51:41 -0500

    path issue

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,22 @@
 require "test_helper"
 
+# rails test test/models/user_test.rb
 class UserTest < ActiveSupport::TestCase
+  test "user has unique fields" do
+    user = User.create(user_params)
 
+    assert_predicate user, :valid?
+  end
+
+  private
+
+  def user_params
+    {
+      name: "Pat Sul",
+      email: "patsul@gmail.com",
+      profile_page: "patsul",
+      password: "password",
+      password_confirmation: "password"
+    }
+  end
 end
